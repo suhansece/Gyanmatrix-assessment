080ccfe857fecf4497143a0322330fc553474850
suhansece <suhansece@gmail.com>
2023-04-11 11:19:14 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const username=[{"gender":"female","name":{"title":"Miss","first":"Peremisla","last":"Guchok"},
+"location":{"street":{"number":5121,"name":"Tamanska"},
+"city":"Horol","state":"Chernivecka","country":"Ukraine","postcode":33932,"coordinates":{"latitude":"76.9824","longitude":"179.4909"},
+"timezone":{"offset":"-12:00","description":"Eniwetok, Kwajalein"}},"email":"peremisla.guchok@example.com",
+"login":{"uuid":"a3308def-423f-4e67-bae7-c5151c88c5bc","username":"heavypeacock402","password":"trance","salt":"hm74zESP","md5":"de0af6bcb0b8e022e4a4a889ca4464f6","sha1":"deceacc493b8816824771f448e1280cd7f6d4604",
+"sha256":"abedba605db9c302e26122205df26b28f4088db859627d671d125ca8f02779fb"},
+"dob":{"date":"1951-02-21T12:42:26.913Z","age":72},"registered":{"date":"2008-07-22T21:40:22.738Z","age":14},
+"phone":"(098) N57-3776","cell":"(066) Z50-8351","id":{"name":"","value":null},
+"picture":{"large":"https://randomuser.me/api/portraits/women/6.jpg","medium":"https://randomuser.me/api/portraits/med/women/6.jpg",
+"thumbnail":"https://randomuser.me/api/portraits/thumb/women/6.jpg"},"nat":"UA"}]
+
+function myFunction() {
+    var x = document.getElementById("name").value;
+    if(x===username.)
+  }
