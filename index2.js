da6952a3cb9a2c58e83e9dc0ecf80a93afa53ba4
suhansece <suhansece@gmail.com>
2023-04-11 19:52:06 +0530

    to display image and name created using api in jsfile

--- index2.js
+++ index2.js
@@ -1,5 +1,3 @@
-const names=document.querySelector(".info-name");
-const image=document.querySelector(".info-image");
 const imagerow=document.querySelector(".info");
 const fetchPokeData = () => {
     fetch('https://hp-api.onrender.com/api/characters/staff')
@@ -8,8 +6,40 @@ const fetchPokeData = () => {
 
        for(var i=0;i<6;i++)
        {   
-           names.textContent=data[i].name;
-           image.src=data[i].image;
+          //create Dom elements
+           const infoName=document.createElement('p');
+           const infodiv=document.createElement('div');
+           const infoImage=document.createElement('img');
+           const infoBackgroudclone=document.createElement('img');
+           const infoFrameclone=document.createElement('img');
+           var f=0;
+           const names=data[i].name
+           for(var j=0;j<names.length;j++){
+            if(names[j]==' '){
+                f=1;
+            }
+            if(f==1){
+                infoName.textContent+=names[j];
+            }
+           }
+           f=0;
+           infoImage.src=data[i].image;
+           //add class list
+           infoImage.classList.add('info-image');
+           infoName.classList.add('info-name');
+           infodiv.classList.add('info-img');
+           infoBackgroudclone.classList.add('info-background');
+           infoFrameclone.classList.add('info-imgframe');
+         //adding src for imgs
+           infoBackgroudclone.src='images/Rectangle 45.png';
+           infoFrameclone.src='images/photo_2023-03-13_20-57-50 1.png';
+           //appendchild
+           infodiv.appendChild(infoImage);
+           infodiv.appendChild(infoName);
+           infodiv.appendChild(infoBackgroudclone);
+           infodiv.appendChild(infoFrameclone);
+           
+           imagerow.appendChild(infodiv);
        }
        
     })}
