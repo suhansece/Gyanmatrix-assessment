ea05b237659b57c773add3df420a7528c3595cd3
suhansece <suhansece@gmail.com>
2023-04-11 20:50:11 +0530

    make login as succesful by api

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-/* 
-const usernames={"gender":"female","name":{"title":"Miss","first":"Peremisla","last":"Guchok"},
-"location":{"street":{"number":5121,"name":"Tamanska"},
-"city":"Horol","state":"Chernivecka","country":"Ukraine","postcode":33932,"coordinates":{"latitude":"76.9824","longitude":"179.4909"},
-"timezone":{"offset":"-12:00","description":"Eniwetok, Kwajalein"}},"email":"peremisla.guchok@example.com",
-"login":{"uuid":"a3308def-423f-4e67-bae7-c5151c88c5bc","username":"heavypeacock402","password":"trance","salt":"hm74zESP","md5":"de0af6bcb0b8e022e4a4a889ca4464f6","sha1":"deceacc493b8816824771f448e1280cd7f6d4604",
-"sha256":"abedba605db9c302e26122205df26b28f4088db859627d671d125ca8f02779fb"},
-"dob":{"date":"1951-02-21T12:42:26.913Z","age":72},"registered":{"date":"2008-07-22T21:40:22.738Z","age":14},
-"phone":"(098) N57-3776","cell":"(066) Z50-8351","id":{"name":"","value":null},
-"picture":{"large":"https://randomuser.me/api/portraits/women/6.jpg","medium":"https://randomuser.me/api/portraits/med/women/6.jpg",
-"thumbnail":"https://randomuser.me/api/portraits/thumb/women/6.jpg"},"nat":"UA"}; */
+ const usernames={"gender":"male","name":{"title":"Mr","first":"Milorad","last":"Simonović"},"location":{"street":{"number":373,"name":"Učitelja Mihailovića"},"city":"Valjevo","state":"Braničevo","country":"Serbia","postcode":58985,"coordinates":{"latitude":"63.2118","longitude":"-63.4221"},"timezone":{"offset":"-8:00","description":"Pacific Time (US & Canada)"}},"email":"milorad.simonovic@example.com","login":{"uuid":"7b20ee08-c8ee-4ba9-a2b5-344c03592b44","username":"silverfrog299","password":"brandy1","salt":"YI45QNLN","md5":"84832da5ab287754c0c1d5b275e3a012","sha1":"6190525ef3c15e85feef57addf69da9bf773deec","sha256":"e5a839aee8c9b822004ea1a609c29a394885e469c24196a0779b154e58e25340"},"dob":{"date":"1961-01-14T03:19:52.618Z","age":62},"registered":{"date":"2007-07-20T23:38:25.274Z","age":15},"phone":"012-3517-223","cell":"062-3354-235","id":{"name":"SID","value":"562675094"},"picture":{"large":"https://randomuser.me/api/portraits/men/75.jpg","medium":"https://randomuser.me/api/portraits/med/men/75.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/75.jpg"},"nat":"RS"}
 
-function myFunction() {
-    alert("*******************");
-      const names=document.querySelector('#name').value;
-      const passwords=document.querySelector('#password').value;
-      if(names==usernames.login.username && passwords==usernames.login.password){
-        alert("if");
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
 
-        window.location.assign("students.html");
-      }
-      else{
-        alert("else");
-      }
-     
-  }
+
+const loginButton=document.querySelector('#loginbutton');
+loginButton.addEventListener("click", () => {
+const names=document.querySelector('#name').value;
+const passwords=document.querySelector('#password').value;
+ if(names === usernames.login.username && passwords === usernames.login.password){
+  window.location.assign("students.html");
+} 
+});
+
+
+  
+   
